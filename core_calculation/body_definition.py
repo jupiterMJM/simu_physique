investigation about wrong behaviour of euler angle => probably need to stop working with quaternion for now and move back to euler matrix.....

core_calculation/body_definition.py
@@ -83,11 +83,9 @@ class Body:
                 initial_angular_velocity = [0, 0, 0]
             print(initial_angular_velocity)
             self.angular_velocity = np.array(initial_angular_velocity, dtype="float64")
-            self.quaternion_angular_velocity = 1/2 * quat_mul(self.local_basis.quaternion, np.array([0, *initial_angular_velocity], dtype="float64"))
+            # self.quaternion_angular_velocity = 1/2 * quat_mul(self.local_basis.quaternion, np.array([0, *initial_angular_velocity], dtype="float64"))
             # self.inertia_matrix = np.array(inertia_matrix, dtype="float64") if inertia_matrix is not None else np.eye(3)
             self.inertia_matrix = np.array([inertia_matrix["e1"], inertia_matrix["e2"], inertia_matrix["e3"]], dtype="float64").T if inertia_matrix is not None else np.eye(3)
-            print(self.inertia_matrix)
-            print(self.quaternion_angular_velocity)
         else:
             self.representation = "point_mass"
             if initial_angular_velocity is not None:

core_calculation/core_simulation.py
@@ -150,13 +150,13 @@ class Simulation:
 
                 if body.representation == "3D_solid_body":
                     # torque = np.array([0, 0, 1]).T
-                    torque = np.array([1, 0, 0]).T
+                    torque_local = np.array([0, 0, 0]).T
                     # it s the derivative of omega => it s the acceleration of the angular basis!!!!!!
-                    body.angular_velocity += np.linalg.inv(body.inertia_matrix) @ (torque  # update angular velocity
+                    body.angular_velocity += np.linalg.inv(body.inertia_matrix) @ (torque_local  # update angular velocity
                         - np.cross(body.angular_velocity, body.inertia_matrix @ body.angular_velocity)
                     )*self.dt
                     dq = R.from_rotvec(body.angular_velocity * self.dt)
-                    body.local_basis._local_basis = body.local_basis._local_basis * dq
+                    body.local_basis._local_basis = dq * body.local_basis._local_basis
                     # print(body.local_basis.euler_angle)
 
 

hands_on/euler_integration_angle.py
@@ -25,14 +25,16 @@ trajectory[0] = q.as_quat(scalar_first=True)
 # ------------------------
 
 # I = np.eye(3)  # matrice d'inertie (exemple : sphère)
-I = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 5]])
-omega = np.array([0.0, 0.0, .1])  # vitesse angulaire initiale
-torque_local = np.array([1, 0.0, 0.0])  # moment constant dans la base locale
+I = np.array([[1, 0, 0], [0, 2, 0], [0, 0, 3]])
+omega = np.array([1, 0.2,0 ])  # vitesse angulaire initiale
+torque_local = np.array([0, 0.0, 0.0])  # moment constant dans la base locale
 
 q = R.from_quat([1, 0, 0, 0])  # quaternion initial
 trajectory = np.zeros((N+1, 4))
 trajectory[0] = q.as_quat(scalar_first=True)
 historique_angles = [q.as_euler('ZYX')]
+
+
 
 for i in range(1, N):
     # Calcul du moment dans la base globale